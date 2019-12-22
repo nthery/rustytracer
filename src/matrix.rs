261f534092b576596e53aae42e83b7a2b976213f
Nicolas Thery <nthery@gmail.com>
2019-12-22 17:28:33 +0100

    add support for 2x2 and 3x3 matrices

--- src/matrix.rs
+++ src/matrix.rs
@@ -31,6 +31,26 @@ impl Matrix {
         }
     }
 
+    /// Create a new 3x3 matrix containing the given values.
+    pub fn new_3x3(values: &[[f64; 3]; 3]) -> Matrix {
+        let cells = values.iter().flatten().cloned().collect::<Vec<f64>>();
+        Matrix {
+            nrows: 3,
+            ncols: 3,
+            cells,
+        }
+    }
+
+    /// Create a new 2x2 matrix containing the given values.
+    pub fn new_2x2(values: &[[f64; 2]; 2]) -> Matrix {
+        let cells = values.iter().flatten().cloned().collect::<Vec<f64>>();
+        Matrix {
+            nrows: 2,
+            ncols: 2,
+            cells,
+        }
+    }
+
     /// Return the value at the specified row and column.
     pub fn get(&self, r: usize, c: usize) -> f64 {
         assert!(r < self.nrows);
@@ -53,5 +73,19 @@ mod tests {
         ]);
         assert_eq!(m.get(0, 0), 1.0);
         assert_eq!(m.get(3, 2), 15.5);
+    }
+
+    #[test]
+    fn constructing_and_inspecting_3x3_matrix() {
+        let m = Matrix::new_3x3(&[[1.0, 2.0, 3.0], [5.5, 6.5, 7.5], [9.0, 10.0, 11.0]]);
+        assert_eq!(m.get(0, 0), 1.0);
+        assert_eq!(m.get(2, 1), 10.0);
+    }
+
+    #[test]
+    fn constructing_and_inspecting_2x2_matrix() {
+        let m = Matrix::new_2x2(&[[1.0, 2.0], [5.5, 6.5]]);
+        assert_eq!(m.get(0, 0), 1.0);
+        assert_eq!(m.get(1, 1), 6.5);
     }
 }
