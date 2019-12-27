8efece3b9d8c40b81ed441216a6311217da14a87
Nicolas Thery <nthery@gmail.com>
2019-12-27 22:27:16 +0100

    add transform::rotation_x()

--- src/transform.rs
+++ src/transform.rs
@@ -22,10 +22,21 @@ pub fn scaling(x: f64, y: f64, z: f64) -> Matrix {
     res
 }
 
+/// Returns matrix encoding rotation of `angle` radiants arond the x axis.
+pub fn rotation_x(angle: f64) -> Matrix {
+    let mut res = Matrix::new_4x4_identity();
+    res.set(1, 1, angle.cos());
+    res.set(1, 2, -angle.sin());
+    res.set(2, 1, angle.sin());
+    res.set(2, 2, angle.cos());
+    res
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::tuple::Tuple;
+    use std::f64::consts::PI;
 
     #[test]
     fn multiplying_by_translation_matrix() {
@@ -87,6 +98,28 @@ mod tests {
         assert_eq!(
             &t * &Tuple::new_vector(2.0, 3.0, 4.0),
             Tuple::new_vector(-2.0, 3.0, 4.0)
+        );
+    }
+
+    #[test]
+    fn rotating_around_x_axis() {
+        let p = Tuple::new_point(0.0, 1.0, 0.0);
+        let half_quarter = rotation_x(PI / 4.0);
+        let full_quarter = rotation_x(PI / 2.0);
+        assert_eq!(
+            &half_quarter * &p,
+            Tuple::new_point(0.0, 2_f64.sqrt() / 2.0, 2_f64.sqrt() / 2.0)
+        );
+        assert_eq!(&full_quarter * &p, Tuple::new_point(0.0, 0.0, 1.0));
+    }
+
+    #[test]
+    fn inverting_x_rotation_rotates_in_opposite_direction() {
+        let p = Tuple::new_point(0.0, 1.0, 0.0);
+        let half_quarter = rotation_x(PI / 4.0);
+        assert_eq!(
+            &half_quarter.inverted() * &p,
+            Tuple::new_point(0.0, 2_f64.sqrt() / 2.0, -2_f64.sqrt() / 2.0)
         );
     }
 }
