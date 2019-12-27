3d149cffada8f70c8a6e0e4599423fb590c5ee2e
Nicolas Thery <nthery@gmail.com>
2019-12-27 23:23:47 +0100

    add Ray::distance()

--- src/ray.rs
+++ src/ray.rs
@@ -23,6 +23,11 @@ impl Ray {
     pub fn direction(&self) -> &Tuple {
         &self.direction
     }
+
+    /// Returns coordinates of point on this ray at distance `t` from origin.
+    pub fn position(&self, t: f64) -> Tuple {
+        &self.origin + &(&self.direction * t)
+    }
 }
 
 #[cfg(test)]
@@ -36,5 +41,18 @@ mod tests {
         let r = Ray::new(o.clone(), d.clone());
         assert_eq!(*r.origin(), o);
         assert_eq!(*r.direction(), d);
+    }
+
+    #[test]
+    fn computing_point_from_distance() {
+        let r = Ray::new(
+            Tuple::new_point(2.0, 3.0, 4.0),
+            Tuple::new_vector(1.0, 0.0, 0.0),
+        );
+
+        assert_eq!(r.position(0.0), Tuple::new_point(2.0, 3.0, 4.0));
+        assert_eq!(r.position(1.0), Tuple::new_point(3.0, 3.0, 4.0));
+        assert_eq!(r.position(-1.0), Tuple::new_point(1.0, 3.0, 4.0));
+        assert_eq!(r.position(2.5), Tuple::new_point(4.5, 3.0, 4.0));
     }
 }
