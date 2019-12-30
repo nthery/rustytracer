3e26497f07a3aa9d0d8f06df145622740a4bdee4
Nicolas Thery <nthery@gmail.com>
2019-12-30 09:25:34 +0100

    add light::Material

--- src/light.rs
+++ src/light.rs
@@ -1,8 +1,8 @@
-//! Light-related abstractions.
+//! Light and shading related abstractions.
 //!
 //! See TRTC chapter 6.
 
-use crate::color::Color;
+use crate::color::{self, Color};
 use crate::tuple::Tuple;
 
 /// A sizeless light source.
@@ -21,16 +21,36 @@ impl PointLight {
     }
 }
 
+/// Phong reflection model parameters.
+pub struct Material {
+    pub color: Color,
+    pub ambient: f64,
+    pub diffuse: f64,
+    pub specular: f64,
+    pub shininess: f64,
+}
+
+impl Default for Material {
+    fn default() -> Self {
+        Material {
+            color: color::WHITE,
+            ambient: 0.1,
+            diffuse: 0.9,
+            specular: 0.9,
+            shininess: 200.0,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::color::WHITE;
     use crate::tuple::ORIGIN;
 
     #[test]
     fn point_light_has_position_and_intensity() {
-        let l = PointLight::new(WHITE, ORIGIN);
-        assert_eq!(l.intensity, WHITE);
+        let l = PointLight::new(color::WHITE, ORIGIN);
+        assert_eq!(l.intensity, color::WHITE);
         assert_eq!(l.position, ORIGIN);
     }
 }
