46584c17f6229c367223ba3697732c80f5bfcb07
Nicolas Thery <nthery@gmail.com>
2020-01-04 17:26:52 +0100

    add Camera::ray_for_pixel()

--- src/camera.rs
+++ src/camera.rs
@@ -3,6 +3,8 @@
 //! See TRTC chapter 7.
 
 use crate::matrix::Matrix;
+use crate::ray::Ray;
+use crate::tuple::{Tuple, ORIGIN};
 
 /// Parameters to map the 3D world to a 2D canvas.
 pub struct Camera {
@@ -65,11 +67,32 @@ impl Camera {
     pub fn pixel_size(&self) -> f64 {
         self.pixel_size
     }
+
+    /// Computes a ray cast from the camera to `(x,y)` on canvas.
+    pub fn ray_for_pixel(&self, x: usize, y: usize) -> Ray {
+        // Compute point offsets from canvas top-left in world units.
+        let x_off = (x as f64 + 0.5) * self.pixel_size;
+        let y_off = (y as f64 + 0.5) * self.pixel_size;
+
+        // Compute coordinates of untransformed point in world space.
+        let x_world = self.half_width - x_off;
+        let y_world = self.half_height - y_off;
+
+        // Compute transformed coordinates.
+        // The canvas is at z = -1.
+        let t = self.transform.inverted();
+        let pixel = &t * &Tuple::new_point(x_world, y_world, -1.0);
+        let origin = &t * &ORIGIN;
+        let direction = (&pixel - &origin).normalized();
+
+        Ray::new(origin, direction)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::transform;
     use crate::util;
     use std::f64::consts::PI;
 
@@ -92,5 +115,36 @@ mod tests {
     fn pixel_size_for_vertical_canvas() {
         let c = Camera::new(125, 200, PI / 2.0);
         assert!(util::nearly_equal(c.pixel_size(), 0.01));
+    }
+
+    #[test]
+    fn constructing_ray_through_canvas_center() {
+        let c = Camera::new(201, 101, PI / 2.0);
+        assert_eq!(
+            c.ray_for_pixel(100, 50),
+            Ray::from_triplets((0.0, 0.0, 0.0), (0.0, 0.0, -1.0))
+        );
+    }
+
+    #[test]
+    fn constructing_ray_through_canvas_border() {
+        let c = Camera::new(201, 101, PI / 2.0);
+        assert_eq!(
+            c.ray_for_pixel(0, 0),
+            Ray::from_triplets((0.0, 0.0, 0.0), (0.66519, 0.33259, -0.66851))
+        );
+    }
+
+    #[test]
+    fn constructing_ray_when_camera_transformed() {
+        let mut c = Camera::new(201, 101, PI / 2.0);
+        c.transform = &transform::rotation_y(PI / 4.0) * &transform::translation(0.0, -2.0, 5.0);
+        assert_eq!(
+            c.ray_for_pixel(100, 50),
+            Ray::from_triplets(
+                (0.0, 2.0, -5.0),
+                (2_f64.sqrt() / 2.0, 0.0, -2_f64.sqrt() / 2.0)
+            )
+        );
     }
 }
