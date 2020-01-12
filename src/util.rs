7227d948917c7b5a16854529ab725e375cac5c92
Nicolas Thery <nthery@gmail.com>
2020-01-12 06:47:18 +0100

    name epsilon used in nearly_equal()

--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,12 @@
 //! Internal utilities.
 
+pub const EPSILON: f64 = 0.00001;
+
 /// Return true if arguments are approximatevely equal.
 /// We use the implementation from TRTC so that we can copy-paste tests easily.
 ///
 /// This is probably not the best implementation.  See for example:
 /// https://users.rust-lang.org/t/assert-eq-for-float-numbers/7034/4
 pub fn nearly_equal(a: f64, b: f64) -> bool {
-    (a - b).abs() < 0.00001
+    (a - b).abs() < EPSILON
 }
